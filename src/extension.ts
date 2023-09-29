6c6aa918c6ac491a49dbc2e3488b2c64ba800443
Dolu <alf.rider@hotmail.fr>
2023-09-29 23:11:59 +0200

    feat: auto detect language

--- src/extension.ts
+++ src/extension.ts
@@ -3,15 +3,26 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
     console.log('Congratulations, your extension "copywithticks" is now active!');
 
-    let copyWithTicks = vscode.commands.registerCommand('extension.copyWithTicks', () => {
+    let copyWithTicks = vscode.commands.registerCommand('extension.copyWithTicks', async () => {
         let editor = vscode.window.activeTextEditor;
+
         if (editor) {
+            // Prompt the user for the file extension, and add the current file extension as placeholder
+
             let document = editor.document;
             let selection = editor.selection;
             let selectedText = document.getText(selection);
-            let modifiedText = `\`\`\`\n${selectedText}\n\`\`\``;
+            let fileExtension = "";
+            const fileName = document.fileName;
+
+            if (fileName.split('.').length > 1) {
+                fileExtension = fileName.split('.').pop() as string;
+            }
+
+            let modifiedText = `\`\`\`${fileExtension}\n${selectedText}\n\`\`\``;
+
             vscode.env.clipboard.writeText(modifiedText);
-            vscode.window.showInformationMessage('Text copied with ticks!');
+            vscode.window.showInformationMessage('Text copied with ticks and file extension!');
         }
     });
 
